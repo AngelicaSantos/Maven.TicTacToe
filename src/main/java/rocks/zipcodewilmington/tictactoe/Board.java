d8b6192b6b5336b742aa814782338390f876176d
AngelicaSantos <angelica.santos2113@gmail.com>
2019-06-05 09:21:29 -0400

    complete

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -1,15 +1,53 @@
 package rocks.zipcodewilmington.tictactoe;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] matrix;
+    int row ;
+    int column;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
+
+        for (int row=0; row<=matrix.length; row++)
+        {
+            Arrays.fill(matrix[row],' ');
+        }
+
     }
 
-    public Boolean isInFavorOfX() {
-        return null;
-    }
+    /**
+     * This method will validate if the players move is allowed and return true if the move is completed.
+     */
+
+    public Boolean makeMove(char player, int row, int column)
+    {
+        if (row >=0 && row <=2 && column >=0 && column <=2) {
+
+            if (matrix[row][column] != ' ')
+                return false;
+
+            else {
+                matrix[row][column] = player;
+                return true;
+            }
+
+            }
+        return false;
+
+    } // end of make move method
+
+
+
+
+    public Boolean isInFavorOfX()
+
+
+
 
     public Boolean isInFavorOfO() {
         return null;
@@ -18,6 +56,7 @@ public class Board {
     public Boolean isTie() {
         return null;
     }
+
 
     public String getWinner() {
         return null;
